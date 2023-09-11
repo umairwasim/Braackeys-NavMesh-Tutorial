9f1c515c27c6400860c2678dc09d5cef695f53b5
Umair <umairwasim001@gmail.com>
2023-09-12 02:29:11 +0500

    Update PlayerController.cs

--- NavMesh Example Project/Assets/Example01 - Basics/PlayerController.cs
+++ NavMesh Example Project/Assets/Example01 - Basics/PlayerController.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using UnityEngine.AI;
-
+using UnityStandardAssets.Characters.ThirdPerson;
 public class PlayerController : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private ThirdPersonCharacter character;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        character = GetComponent<ThirdPersonCharacter>();
+        agent.updateRotation = false;
     }
 
     // Update is called once per frame
@@ -16,5 +19,11 @@ public class PlayerController : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit raycastHit))
                 agent.SetDestination(raycastHit.point);
+
+        if (agent.remainingDistance > agent.stoppingDistance)
+            character.Move(agent.desiredVelocity, false, false);
+        else
+            character.Move(Vector3.zero, false, false);
+
     }
 }
